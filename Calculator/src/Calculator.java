d35df2711ea71f77fd6a05ffec8685c327844615
MUHAMMAD RASHIF ARSYUBARRU <i16088@ftis>
2018-11-02 14:39:45 +0700

    fungsi kali dan bagi sudah dibuat

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -11,15 +11,19 @@
 public class Calculator {
 
     public float plus(float a, float b){
-    
+        
+        
     }
     public float minus(float a, float b){
-    
+        
+        
     }
     public float multiply(float a, float b){
-    
+        return a*b;
+        
     }
     public float division(float a, float b){
-    
+        return a/b;
+        
     }
 }
